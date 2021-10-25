ea4e8812babbbeea8f59faea8115c8282197fe6b
Nelsyda Perez <49159549+nelsydaperez@users.noreply.github.com>
2021-10-25 06:38:27 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
+
